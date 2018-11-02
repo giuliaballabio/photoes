edb07d5b1a3933f52631591bb552c0540a6a2a11
Giulia Ballabio <gb258@leicester.ac.uk>
2018-11-02 15:23:20 +0000

    Implement Cathie's method

--- src/selfsimilar_solutions.f90
+++ src/selfsimilar_solutions.f90
@@ -1,126 +1,128 @@
-program selfsimilarity
+
+! ---------------------------------------------------------------------------------------
+!
+!          Giulia Ballabio, gb258@leicester.ac.uk
+!          Created on June 2018
+!
+!        Compute the semi-analythical solution for photoevaporative winds
+!            from Clarke and Alexander, 2016
+!            This version has been created following Cathie's code ssbasegb.f
+!
+!         N.B. run the code with these flags:
+!        gfortran -Wunused-variable -Wextra -ffpe-trap=invalid,zero,overflow
+!           -finit-real=snan -pedantic -fbounds-check -g -o output program.f90
+!
+! ---------------------------------------------------------------------------------------
+
+program selfsimilar
 
 implicit none
 
-integer                          :: i,n
-real, dimension(1:50001)         :: x,y,u,dx,du,dA,ddx
-real                             :: du_dy,dA_dy,ddx_dy
-!! BIN WIDTH
-real, parameter                  :: dy=0.0001,ymax=5.
-!! DIMENSIONAL VARIABLES
-real, parameter                  :: cs=1.e5,ub=0.85*cs,b=0.75
-integer                          :: infile
-logical,save                     :: init=.false.
+integer                                          :: i
+integer,parameter                                :: n=5000000
+double precision,parameter                       :: cs=1.d6
+double precision                                 :: b,reff0,gm
+double precision                                 :: x0,y0,u0,dx,dy
+double precision                                 :: x1,y1,u1,tanth1,a1,x2,y2,u2,tanth2,a2
+double precision                                 :: ft,rhs1,rhs2,rhs3,rh,rhs,lhs1,lhs2,lhs
+double precision                                 :: dudy,ddx1,ddx2,ddxdy
+double precision                                 :: dphi,dphidy,gefft
+external                                         :: derivs
 
-n=int(ymax/dy)
+!ub=(0.92,0.85,0.77,0.56,0.29)
+!b=(0.5,0.75,1.,1.5,2.)
 
-!! INITIAL CONDITIONS AT P
-x(1)=1.
-y(1)=0.0
-u(1)=ub/cs
-dx(1)=0.
+!! INITIAL CONDITIONS
+x0=1.
+y0=1.
+u0=0.85
+b=0.75
+! I don't know what this parameter does,but it's very important!
+! It should be the gravity set to zero
+gm=0.0 !1.d-6
+
+reff0=u0*u0*x0/b
+
+dx=1.d-8
+dy=1.d-6
+
+x1=x0+dx
+y1=(2.*reff0*dx-dx*dx)**0.5
+u1=u0
+
+tanth1=(reff0-dx)/y1
+!a1=(x1*x1*tanth1-x1*y1)/(1.+tanth1*tanth1)**0.5
 
 do i=1,n
-    du(i)=du_dy(x(i),y(i),u(i),dx(i))
-    y(i+1)=y(i)+dy
-    !! EULER METHOD TO CALCULATE u
-    u(i+1)=u(i)+dy*du(i)
-    !! CALCULTE dA AND ddx FROM EQS. 14 & 15
-    dA(i)=dA_dy(x(i),y(i),u(i),dx(i))
-    ddx(i)=ddx_dy(x(i),y(i),u(i),dx(i),dA(i))
-    !! CALCULATE x AND dx AT THE SUBSEQUENT POINT
-    dx(i+1)=dx(i)+ddx(i)*dy
-    x(i+1)=x(i)+dx(i)*dy+0.5*ddx(i)*dy*dy
+ft=(1.+tanth1*tanth1)**0.5
+a1=(x1*x1*tanth1-x1*y1)/(1.+tanth1*tanth1)**0.5
+rhs1=b*ft/(x1*tanth1-y1)
+rhs2=-u1**2.*(x1*tanth1-y1)/(ft*x1*(x1+y1*tanth1))
+rhs3=u1*u0*tanth1*EXP(0.5*(u1**2.-u0**2.)+dphi(x1,y1,gm))*dphidy(x1,y1,tanth1,gm) &
+/(x1*(x1+y1*tanth1))
+rh=-tanth1*((x1+y1*tanth1)*dphidy(x1,y1,tanth1,gm))/(ft*(x1*tanth1-y1))
+rhs=rhs1+rhs2+rhs3+rh+gefft(x1,y1,tanth1,gm)
+lhs1=a1*u0*(1.-u1**2.)*tanth1*EXP(0.5*(u1**2.-u0**2.) &
++dphi(x1,y1,gm))/(x1*(x1+y1*tanth1))
+lhs2=u1*tanth1*(x1+y1*tanth1)/(ft*(x1*tanth1-y1))
+lhs=lhs1+lhs2
+dudy=rhs/lhs
+ddx1=a1*u0*((u1**(-2.)-1.)*dudy+(dphidy(x1,y1,tanth1,gm)/u1))*EXP(0.5*(u1**2.-u0**2.) &
++dphi(x1,y1,gm))*ft**3./(tanth1**2.*x1*(x1+y1*tanth1))
+ddx2=ft**2.*(x1*tanth1-y1)/(tanth1**3.*x1*(x1+y1*tanth1))
+ddxdy=ddx1+ddx2
+a2=(x1**2.*tanth1-x1*y1)/(1.+tanth1**2.)**0.5
+x2=x1+dy/tanth1+0.5*ddxdy*dy**2.
+y2=y1+dy
+u2=u1+dudy*dy
+tanth2=(1./tanth1+ddxdy*dy)**(-1.)
+open(unit=8,file='b0.75.txt')
+write(8,'(2(es18.10,1X))') x2,y2
+x1=x2
+y1=y2
+u1=u2
+tanth1=tanth2
+a1=a2
 enddo
-
-if (.not.init) then
-    open(infile,file='output.txt')
-    !write(infile,"('# ',2('[',i2.2,1x,a12,']',1x))") 1,'x',2,'y'
-else
-    open(infile,file='output.txt',status='old',position='append')
-endif
-do i=1,n
-    write(infile,'(2(es18.10,1X))') x(i),y(i)
-enddo
-close(infile)
 
 end program
 
 !--------------------------------------------------------------------------
-! Useful Functions
+! Functions
 !--------------------------------------------------------------------------
 
-real function f(x,y,u,dx)
+double precision function dphi(x,y,gm)
 
-real                             :: x,y,u,dx
-real                             :: c1,c2,e,d
-real, parameter                  :: cs=1.e5,ub=0.85*cs
+integer,parameter                                :: n=12000000
+double precision                                 :: x,y,r
+double precision                                 :: gm
 
-c1=-(ub**2*u/cs)**2
-e=0.5*(ub/cs)**2*(u**2-1.)
-c2=u*ub**2
-d=dx*(x*dx+y)
+r=(x**2.+y**2.)**0.5
+dphi=gm*(-r**(-1.)+0.5*(x)**(-2.)+0.5)
+return
+end function dphi
 
-if (d.eq.0) then
-    f=(ub*ub/cs)**2
-else
-    f=(c1/d)*EXP(e)*(1.-(cs/(ub*u))**2)+c2*(x*dx+y)/((1.+dx**2)**0.5*(x-y*dx))
-endif
+double precision function dphidy(x,y,tanth,gm)
 
-end function f
+integer,parameter                                :: n=12000000
+double precision                                 :: x,y,tanth,r
+double precision                                 :: gm
 
-real function g(x,y,u,dx)
+r=(x**2.+y**2.)**0.5
+dphidy=gm*(((x+y*tanth)/(tanth*r**3.))-(1./(tanth*x**3.)))
+return
+end function dphidy
 
-real                             :: x,y,u,dx
-real                             :: c1,c2,d
-real, parameter                  :: cs=1.e5,ub=0.85*cs,b=0.75
+double precision function gefft(x,y,tanth,gm)
 
-c1=b*cs**2
-c2=(ub*u)**2
-d=(1+x**2)**0.5*x*(x*dx+y)
+integer,parameter                                :: n=12000000
+double precision                                 :: x,y,tanth,r,ft
+double precision                                 :: gm
 
-if (d.eq.0) then
-    g=b*(cs**2)
-else
-    g=c1*(1.+dx**2)**0.5/(x-y*dx)-c2*dx*(x-y*dx)/d
-endif
+r=(x**2.+y**2.)**0.5
+ft=(1.+tanth**2.)**0.5
+gefft=gm*((tanth/(ft*x**3.))-((x*tanth-y)/(ft*r**3.)))
+return
+end function gefft
 
-end function g
 
-real function du_dy(x,y,u,dx)
-
-real                             :: x,y,u,dx
-
-du_dy=g(x,y,u,dx)/f(x,y,u,dx)
-
-end function du_dy
-
-real function dA_dy(x,y,u,dx)
-
-real                             :: x,y,u,dx
-real                             :: a,e
-real, parameter                  :: cs=1.e5,ub=0.85*cs,b=0.75
-
-a=(ub/cs)**2-(1/u)**2
-e=0.5*(ub/cs)**2*(u**2-1.)
-
-dA_dy=a*du_dy(x,y,u,dx)*EXP(e)
-
-end function dA_dy
-
-real function ddx_dy(x,y,u,dx,dA)
-
-real                             :: x,y,u,dx,dA
-real                             :: d
-
-d=x*y*(1.+(dx**2))+dx !x*(y+x*dx)
-
-if (d.eq.0) then
-    ddx_dy=1/x     
-    !487.089
-    !0.859871e4
-else
-    ddx_dy=((1.+dx)**2*dx*(x-y*dx)-(1.+dx**2)**1.5*dA)/d
-endif
-
-end function ddx_dy
