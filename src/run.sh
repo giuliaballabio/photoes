de983a47cdada0369a8927ab113905756047d26b
Giulia Ballabio <gb258@leicester.ac.uk>
2018-12-05 18:02:01 +0000

    Minor corrections

--- src/run.sh
+++ src/run.sh
@@ -45,7 +45,8 @@ for ((i=0;i<${#array_b[@]};++i)); do
 			rm *genmod* photoes selfsimilar_solutions
 
 			# Submit the job on dial or alice
-			qsub $RUNDIR/../data_b${array_b[i]}\_r$r_input/incl_$incl/submit-job-alice
+			cd $RUNDIR/../data_b${array_b[i]}\_r$r_input/incl_$incl
+			qsub submit-job-alice
 			#qsub $RUNDIR/../submit-job-dial
 
 		done
