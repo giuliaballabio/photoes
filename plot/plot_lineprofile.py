a18a4247afaf1ccd0930766b04f4e57ccea39c5f
Giulia Ballabio <gb258@leicester.ac.uk>
2019-03-19 15:09:21 +0000

    Minor changes to paths

--- plot/plot_lineprofile.py
+++ plot/plot_lineprofile.py
@@ -7,36 +7,36 @@ import matplotlib.pyplot as plt
 # r_in = input("Insert the inner radius: ")
 # r_out = input("The outer radius: ")
 # cs = input("And the sound speed: ")
-b = [0.75, 1.00, 1.50, 2.00]
-incl_deg = 90.0 #[0.0, 45.0, 90.0]
+b = 0.75 #[0.75, 1.00, 1.50, 2.00]
+incl_deg = [0.0, 20.0, 45.0, 60.0, 90.0]
 r_in = 0.1 #[0.03, 0.1, 1.0]
 r_out = 1.0 #[5.0, 9.5]
 cs = 10 #3 5
 
-quantity = b
+quantity = incl_deg
 v = []
 line_flux = []
 plt.figure()
 for i in range(len(quantity)):
-    v = -np.array(map(float, [lines.split()[0] for lines in open('../cs'+str(cs)+'kms/data_b'+str('{:.2f}'.format(round(b[i], 2)))+'_r'+str(r_in)+'_r'+str(r_out)+'/incl_'+str(round(incl_deg, 2))+'/line_profile_i'+str(round(incl_deg, 2))+'.txt', 'r')]))
-    line_flux = np.array(map(float, [lines.split()[1] for lines in open('../cs'+str(cs)+'kms/data_b'+str('{:.2f}'.format(round(b[i], 2)))+'_r'+str(r_in)+'_r'+str(r_out)+'/incl_'+str(round(incl_deg, 2))+'/line_profile_i'+str(round(incl_deg, 2))+'.txt', 'r')]))
+    v = -np.array(map(float, [lines.split()[0] for lines in open('../cs'+str(cs)+'kms/data_b'+str('{:.2f}'.format(round(b, 2)))+'_r'+str(r_in)+'_r'+str(r_out)+'/incl_'+str(round(incl_deg[i], 2))+'/line_profile_i'+str(round(incl_deg[i], 2))+'.txt', 'r')]))
+    line_flux = np.array(map(float, [lines.split()[1] for lines in open('../cs'+str(cs)+'kms/data_b'+str('{:.2f}'.format(round(b, 2)))+'_r'+str(r_in)+'_r'+str(r_out)+'/incl_'+str(round(incl_deg[i], 2))+'/line_profile_i'+str(round(incl_deg[i], 2))+'.txt', 'r')]))
 
     c = [float(i)/float(len(quantity)), 0.0, float(len(quantity)-i)/float(len(quantity))]
     # if incl_deg[i] == 90.0:
     #     v = -1. * v
     # else:
     #     v = v
-    plt.plot(v, line_flux / np.amax(line_flux), color=c, label='b='+str(quantity[i]))
+    plt.plot(v, line_flux / np.amax(line_flux), color=c, label='i='+str(quantity[i]))
     # plt.vlines(0., 0., 1.3, 'k', linestyle='--')
     plt.title(r'Line profile', fontsize=15)
     plt.xlabel(r'v [$\frac{km}{s}$]', fontsize=15)
     plt.ylabel(r'Normalized L(v)', fontsize=15)
     # plt.axis([-40., 40., 0., 1.2])
     plt.legend(loc='best')
-    plt.savefig('./line_profile_i'+str(round(incl_deg, 2))+'_r'+str(r_in)+'_r'+str(r_out)+'_cs'+str(cs)+'.png', format='png', bbox_inches='tight')
-    # plt.savefig('./lineprofiles/line_profile_b'+str('{:.2f}'.format(round(b, 2)))+'_r'+str(r_in)+'_r'+str(r_out)+'.png', format='png', bbox_inches='tight')
-    # plt.savefig('./line_profile_b'+str('{:.2f}'.format(round(b, 2)))+'_i'+str(round(incl_deg, 2))+'_r'+str(r_out)+'.png', format='png', bbox_inches='tight')
-    # plt.savefig('./line_profile_b'+str('{:.2f}'.format(round(b, 2)))+'_i'+str(round(incl_deg, 2))+'_r'+str(r_in)+'.png', format='png', bbox_inches='tight')
+    # plt.savefig('./lineprofiles/line_profile_i'+str(round(incl_deg, 2))+'_r'+str(r_in)+'_r'+str(r_out)+'_cs'+str(cs)+'.png', format='png', bbox_inches='tight')
+    plt.savefig('./lineprofiles/line_profile_b'+str('{:.2f}'.format(round(b, 2)))+'_r'+str(r_in)+'_r'+str(r_out)+'.png', format='png', bbox_inches='tight')
+    # plt.savefig('./lineprofiles/line_profile_b'+str('{:.2f}'.format(round(b, 2)))+'_i'+str(round(incl_deg, 2))+'_r'+str(r_out)+'.png', format='png', bbox_inches='tight')
+    # plt.savefig('./lineprofiles/line_profile_b'+str('{:.2f}'.format(round(b, 2)))+'_i'+str(round(incl_deg, 2))+'_r'+str(r_in)+'.png', format='png', bbox_inches='tight')
 plt.show()
 
 # v = np.array(map(float, [lines.split()[0] for lines in open('../data_b'+str('{:.2f}'.format(round(b, 2)))+'_r'+str(r_in)+'_r'+str(r_out)+'/incl_'+str(round(incl_deg, 2))+'/line_profile_i'+str(round(incl_deg, 2))+'.txt', 'r')]))
