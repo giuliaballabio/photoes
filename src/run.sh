5e44818676bc7774457df45d2189168b6cd8f8a0
Giulia Ballabio <gb258@leicester.ac.uk>
2019-03-29 14:46:58 +0000

    Correct the path to introduce the species

--- src/run.sh
+++ src/run.sh
@@ -3,17 +3,18 @@
 
 RUNDIR=${PWD}
 
-array_b=( 0.75 1.00 1.50 2.00 )
-array_ub=( 0.85 0.77 0.56 0.29 )
+array_b=( 0.75 ) #1.00 1.50 2.00 )
+array_ub=( 0.85 ) #0.77 0.56 0.29 )
 
 cs=10.0d5 #5.0d5 10.d5
 string_cs=10 #5 10
+species=SII
 
 for ((i=0;i<${#array_b[@]};++i)); do
   # echo "(${array_b[i]}, ${array_ub[i]})"
 	for r_inner in 0.1; do
-		for r_outer in 0.9 1.0 1.5; do
-			for incl in 27.0 35.0 45.0 50.0 60.0 68.0 75.0 82.0; do
+		for r_outer in 9.5; do
+			for incl in 27.0; do #35.0 45.0 50.0 60.0 68.0 75.0 82.0; do
 
 				echo "up to b="${array_b[i]}", R_out="$r_outer "and R_in="$r_inner "for i="$incl
 
@@ -39,31 +40,29 @@ for ((i=0;i<${#array_b[@]};++i)); do
 				# gfortran -Wunused-variable -Wextra -ffpe-trap=invalid,zero,overflow -pedantic -finit-real=snan -fbounds-check -g -fopenmp -o selfsimilar_solutions selfsimilar_solutions.f90
 				# gfortran -Wunused-variable -Wextra -ffpe-trap=invalid,zero,overflow -pedantic -finit-real=snan -fbounds-check -g -fopenmp -o photoes line_profile.f90
 
-				# Make a new directory if it doesn't exist
-				# if [ ! -d $RUNDIR/../data_b${array_b[i]}\_r$r_inner ]; then
-				# 	mkdir $RUNDIR/../data_b${array_b[i]}\_r$r_inner
-				# fi
-				#
-				# cd $RUNDIR/../data_b${array_b[i]}\_r$r_inner
-				if [ ! -d $RUNDIR/../cs$string_cs\kms/data_b${array_b[i]}\_r$r_inner\_r$r_outer/incl_$incl ]; then
-					if [ ! -d $RUNDIR/../cs$string_cs\kms/data_b${array_b[i]}\_r$r_inner\_r$r_outer ]; then
-						if [ ! -d $RUNDIR/../cs$string_cs\kms ]; then
-							mkdir $RUNDIR/../cs$string_cs\kms
+				if [ ! -d $RUNDIR/../cs$string_cs\kms/$species/data_b${array_b[i]}\_r$r_inner\_r$r_outer/incl_$incl ]; then
+					if [ ! -d $RUNDIR/../cs$string_cs\kms/$species/data_b${array_b[i]}\_r$r_inner\_r$r_outer ]; then
+						if [ ! -d $RUNDIR/../cs$string_cs\kms/$species ]; then
+							if [ ! -d $RUNDIR/../cs$string_cs\kms ]; then
+								mkdir $RUNDIR/../cs$string_cs\kms
+							fi
+							mkdir $RUNDIR/../cs$string_cs\kms/$species
 						fi
-						mkdir $RUNDIR/../cs$string_cs\kms/data_b${array_b[i]}\_r$r_inner\_r$r_outer
+						mkdir $RUNDIR/../cs$string_cs\kms/$species/data_b${array_b[i]}\_r$r_inner\_r$r_outer
 					fi
-					mkdir $RUNDIR/../cs$string_cs\kms/data_b${array_b[i]}\_r$r_inner\_r$r_outer/incl_$incl
+					mkdir $RUNDIR/../cs$string_cs\kms/$species/data_b${array_b[i]}\_r$r_inner\_r$r_outer/incl_$incl
 				fi
 
 
+
 				# Copy files to this new directory
-				cp selfsimilar_solutions photoes submit-job* $RUNDIR/../cs$string_cs\kms/data_b${array_b[i]}\_r$r_inner\_r$r_outer/incl_$incl
+				cp selfsimilar_solutions photoes submit-job* $RUNDIR/../cs$string_cs\kms/$species/data_b${array_b[i]}\_r$r_inner\_r$r_outer/incl_$incl
 
 				# Remove unuseful files
 				rm *genmod* photoes selfsimilar_solutions
 
 				# Submit the job on dial or alice
-				cd $RUNDIR/../cs$string_cs\kms/data_b${array_b[i]}\_r$r_inner\_r$r_outer/incl_$incl
+				cd $RUNDIR/../cs$string_cs\kms/$species/data_b${array_b[i]}\_r$r_inner\_r$r_outer/incl_$incl
 				rm photoes_b*
 				qsub submit-job-alice
 				#qsub $RUNDIR/../submit-job-dial
