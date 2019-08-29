9ac79d2eff3481f28ec502571d70ff3017c38833
Giulia Ballabio <gb258@leicester.ac.uk>
2019-08-29 11:47:19 +0100

    Minor modifications at plot

--- plot/plot_observables_data.py
+++ plot/plot_observables_data.py
@@ -213,10 +213,10 @@ plt.plot(incl_deg, fwhm2, color='#fd8d3c', linestyle='-', linewidth=2.5, marker=
 plt.plot(incl_deg, fwhm3, color='#f03b20', linestyle='-', linewidth=2.5, marker='None', markeredgecolor='#f03b20', label='$b='+str(b[2])+'$')
 plt.plot(incl_deg, fwhm4, color='#bd0026', linestyle='-', linewidth=2.5, marker='None', markeredgecolor='#bd0026', label='$b='+str(b[3])+'$')
 #plt.plot(incl_hydro, fwhm_hydro, color='k', linestyle='dotted', label='$Alexander \, (2008)$')
-plt.errorbar(incl_data, np.abs(fwhm_data), yerr=err_fwhm, markerfacecolor='#bdbdbd', markeredgecolor='None', linestyle='None', marker='o', capsize=3, label='$NC$')
+plt.errorbar(incl_data, np.abs(fwhm_data), yerr=err_fwhm, color='#bdbdbd', markeredgecolor='None', linestyle='None', marker='o', capsize=3, label='$NC$')
 for i in range(len(ID)):
     plt.annotate(ID[i], (incl_data[i]+0.3, np.abs(fwhm_data[i])+0.3))
-plt.errorbar(incl_data, np.abs(fwhm_data1), yerr=err_fwhm1, markerfacecolor='#252525', markeredgecolor='None', linestyle='None', marker='*', capsize=3, label='$BC$')
+plt.errorbar(incl_data, np.abs(fwhm_data1), yerr=err_fwhm1, color='#252525', markeredgecolor='None', linestyle='None', marker='*', capsize=3, label='$BC$')
 for i in range(len(ID)):
     plt.annotate(ID[i], (incl_data[i]+0.3, np.abs(fwhm_data1[i])+0.3))
 # plt.errorbar(incl_data2, np.abs(fwhm_data2), yerr=err_fwhm2, color='gray', linestyle='None', marker='d', capsize=3, label='$Rigliaco\,et\,al.\,(2013)$')
@@ -237,7 +237,7 @@ plt.plot(incl_deg, fwhm2, color='#fd8d3c', linestyle='-', linewidth=2.5, marker=
 plt.plot(incl_deg, fwhm3, color='#f03b20', linestyle='-', linewidth=2.5, marker='None', markeredgecolor='#f03b20', label='$b='+str(b[2])+'$')
 plt.plot(incl_deg, fwhm4, color='#bd0026', linestyle='-', linewidth=2.5, marker='None', markeredgecolor='#bd0026', label='$b='+str(b[3])+'$')
 #plt.plot(incl_hydro, fwhm_hydro, color='k', linestyle='dotted', label='$Alexander \, (2008)$')
-plt.errorbar(incl_data, np.abs(fwhm_data), yerr=err_fwhm, markerfacecolor='#bdbdbd', markeredgecolor='None', linestyle='None', marker='o', capsize=3, label='$NC$')
+plt.errorbar(incl_data, np.abs(fwhm_data), yerr=err_fwhm, color='#bdbdbd', linestyle='None', marker='o', capsize=3, label='$NC$')
 for i in range(len(ID)):
     plt.annotate(ID[i], (incl_data[i]+0.5, np.abs(fwhm_data[i])+0.5))
 plt.xlabel(r'$i \, [^{\circ}]$', fontsize=15)
