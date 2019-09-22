33807c1d5166bbd49c8deeee552f0bc9f63e2927
Giulia Ballabio <gb258@leicester.ac.uk>
2019-09-22 17:59:36 +0100

    Minor changes

--- plot/plot_comparisons_data.py
+++ plot/plot_comparisons_data.py
@@ -198,7 +198,7 @@ for i in range(len(name)):
     plt.annotate(name[i], (incl_data2[i]+0.3, np.abs(vpeak_data2[i])+0.3))
 plt.errorbar(incl_data3, np.abs(vpeak_data3), yerr=err_vpeak3, color='k', linestyle='None', marker='d', capsize=3, label='$Baldovin-Saavedra\,(2012)$')
 for i in range(len(name3)):
-    plt.annotate(name3[i], (incl_data3[i]-10.0, np.abs(vpeak_data3[i])+0.3))
+    plt.annotate(name3[i], (incl_data3[i]-12.0, np.abs(vpeak_data3[i])-0.1))
 plt.errorbar(incl_data, np.abs(vpeak_data), yerr=err_vpeak, color='k', linestyle='None', marker='o', capsize=3, label='$Sacco\,et\,al.\,(2012)$')
 for i in range(len(ID)):
     plt.annotate(ID[i], (incl_data[i]+0.3, np.abs(vpeak_data[i])+0.3))
