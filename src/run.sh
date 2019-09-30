80eea55e662a580324b3833c486912affebb1588
Giulia Ballabio <gb258@leicester.ac.uk>
2019-09-30 14:28:49 +0100

    Minor change

--- src/run.sh
+++ src/run.sh
@@ -6,15 +6,17 @@ RUNDIR=${PWD}
 array_b=( 0.75 1.00 1.50 ) #2.00 )
 array_ub=( 0.85 0.77 0.56 ) #0.29 )
 
-cs=10.0d5 #5.0d5 10.d5
-string_cs=10 #5 10
-species=NeII
+cs=10.0d5
+string_cs=10
+species=SIIc_newvphi # REMEMBER to change it also in line_profile.f90
+mdot=mdot10e-9 # REMEMBER to change it also in line_profile.f90
 
 for ((i=0;i<${#array_b[@]};++i)); do
   # echo "(${array_b[i]}, ${array_ub[i]})"
 	for r_inner in 0.1; do
-		for r_outer in 5.0 5.5 6.0 6.5 7.0 7.5 8.0 8.5 9.0; do
-			for incl in 90.0; do
+		for r_outer in 9.5; do
+			#for incl in 0.0 5.0 10.0 15.0 20.0 25.0 30.0 35.0 40.0 45.0 50.0 55.0 60.0 65.0 70.0 75.0 80.0 85.0 90.0; do
+			for incl in 0.0 20.0 45.0 60.0 75.0 90.0; do
 
 				echo "up to b="${array_b[i]}", R_out="$r_outer "and R_in="$r_inner "for i="$incl
 
@@ -41,32 +43,30 @@ for ((i=0;i<${#array_b[@]};++i)); do
 				# gfortran -Wunused-variable -fdefault-real-8 -fdefault-double-8 -Wextra -ffpe-trap=invalid,zero,overflow -pedantic -finit-real=snan -fbounds-check -g -fopenmp -o selfsimilar_solutions selfsimilar_solutions.f90
 				# gfortran -Wunused-variable -fdefault-real-8 -fdefault-double-8 -Wextra -ffpe-trap=invalid,zero,overflow -pedantic -finit-real=snan -fbounds-check -g -fopenmp -o photoes line_profile.f90
 
-				if [ ! -d $RUNDIR/../cs$string_cs\kms/$species/mdot10e-8/data_b${array_b[i]}\_r$r_inner\_r$r_outer/incl_$incl ]; then
-					if [ ! -d $RUNDIR/../cs$string_cs\kms/$species/mdot10e-8/data_b${array_b[i]}\_r$r_inner\_r$r_outer ]; then
-						if [ ! -d $RUNDIR/../cs$string_cs\kms/$species/mdot10e-8 ]; then
+				if [ ! -d $RUNDIR/../cs$string_cs\kms/$species/$mdot/data_b${array_b[i]}\_r$r_inner\_r$r_outer/incl_$incl ]; then
+					if [ ! -d $RUNDIR/../cs$string_cs\kms/$species/$mdot/data_b${array_b[i]}\_r$r_inner\_r$r_outer ]; then
+						if [ ! -d $RUNDIR/../cs$string_cs\kms/$species/$mdot ]; then
 							if [ ! -d $RUNDIR/../cs$string_cs\kms/$species ]; then
 								if [ ! -d $RUNDIR/../cs$string_cs\kms ]; then
 									mkdir $RUNDIR/../cs$string_cs\kms
 								fi
 								mkdir $RUNDIR/../cs$string_cs\kms/$species
 							fi
-							mkdir $RUNDIR/../cs$string_cs\kms/$species/mdot10e-8
+							mkdir $RUNDIR/../cs$string_cs\kms/$species/$mdot
 						fi
-						mkdir $RUNDIR/../cs$string_cs\kms/$species/mdot10e-8/data_b${array_b[i]}\_r$r_inner\_r$r_outer
+						mkdir $RUNDIR/../cs$string_cs\kms/$species/$mdot/data_b${array_b[i]}\_r$r_inner\_r$r_outer
 					fi
-					mkdir $RUNDIR/../cs$string_cs\kms/$species/mdot10e-8/data_b${array_b[i]}\_r$r_inner\_r$r_outer/incl_$incl
+					mkdir $RUNDIR/../cs$string_cs\kms/$species/$mdot/data_b${array_b[i]}\_r$r_inner\_r$r_outer/incl_$incl
 				fi
 
-
-
 				# Copy files to this new directory
-				cp selfsimilar_solutions photoes submit-job* $RUNDIR/../cs$string_cs\kms/$species/mdot10e-8/data_b${array_b[i]}\_r$r_inner\_r$r_outer/incl_$incl
+				cp selfsimilar_solutions photoes submit-job* $RUNDIR/../cs$string_cs\kms/$species/$mdot/data_b${array_b[i]}\_r$r_inner\_r$r_outer/incl_$incl
 
 				# Remove unuseful files
 				rm *genmod* photoes selfsimilar_solutions
 
 				# Submit the job on dial or alice
-				cd $RUNDIR/../cs$string_cs\kms/$species/mdot10e-8/data_b${array_b[i]}\_r$r_inner\_r$r_outer/incl_$incl
+				cd $RUNDIR/../cs$string_cs\kms/$species/$mdot/data_b${array_b[i]}\_r$r_inner\_r$r_outer/incl_$incl
 				qsub submit-job-alice
 				#qsub $RUNDIR/../submit-job-dial
 				cd $RUNDIR
