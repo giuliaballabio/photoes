2c711c64c9094516486fa46f07dd0ad855addb02
Giulia Ballabio <gb258@leicester.ac.uk>
2019-10-04 18:16:36 +0100

    Managing errors

--- plot/plot_comparisons_data.py
+++ plot/plot_comparisons_data.py
@@ -799,13 +799,13 @@ err_ratio_down = ((np.abs(err_fwhm_down)/np.abs(vcentr_down))**2. + (np.abs(err_
 ## Another way of defining the error bars
 err_min = (np.abs(fwhm_data)-np.abs(err_fwhm))/(np.abs(vcentr_data)+np.abs(err_vcentr))
 err_max = (np.abs(fwhm_data)+np.abs(err_fwhm))/(np.abs(vcentr_data)-np.abs(err_vcentr))
-err_ratio = np.array([err_min, err_max])
+err_ratio = np.array([np.abs(err_min), np.abs(err_max)])
 err_min_sx = (np.abs(fwhm_sx)-np.abs(err_fwhm_sx))/(np.abs(vcentr_sx)+np.abs(err_vcentr_sx))
 err_max_sx = (np.abs(fwhm_sx)+np.abs(err_fwhm_sx))/(np.abs(vcentr_sx)-np.abs(err_vcentr_sx))
-err_ratio_sx = np.array([err_min_sx, err_max_sx])
+err_ratio_sx = np.array([np.abs(err_min_sx), np.abs(err_max_sx)])
 err_min_down = (np.abs(fwhm_down)-np.abs(err_fwhm_down))/(np.abs(vcentr_down)+np.abs(err_vcentr_down))
 err_max_down = (np.abs(fwhm_down)+np.abs(err_fwhm_down))/(np.abs(vcentr_down)-np.abs(err_vcentr_down))
-err_ratio_down = np.array([err_min_down, err_max_down])
+err_ratio_down = np.array([np.abs(err_min_down), np.abs(err_max_down)])
 
 plt.figure()
 plt.plot(incl_deg, fwhm1/(np.abs(v_centr1)+1.e-8), color='#c6dbef', linestyle='-', linewidth=2.5, marker='None', markeredgecolor='#c6dbef', label='$c_{s} = 3 \, km/s$')
