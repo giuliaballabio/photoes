2d258159669ce200a34a36f9c92abad517510233
Giulia Ballabio <gb258@leicester.ac.uk>
2019-11-14 21:05:28 +0000

    Create subplots for paper

--- plot/plot_comparisons_data.py
+++ plot/plot_comparisons_data.py
@@ -464,42 +464,52 @@ fig, ax = plt.subplots(2, 3, sharex='col', sharey='row')
 ax[0,0].plot(incl_deg, np.abs(v_peak3), color='#08306b', linestyle='-', linewidth=2.5, marker='None', markeredgecolor='#08306b')
 ax[0,0].fill_between(incl_deg, bottom_vpeak3, top_vpeak3, color='#08306b', alpha=0.3)
 ax[0,0].set_ylabel(r'$- v_{peak} \, [km/s]$')
-ax[0,0].axis([-1., 91., -0.5, 17.5])
+leg1 = ax[0,0].legend(loc='upper right')
 ax[0,1].plot(incl_deg, np.abs(v_peak2), color='#2171b5', linestyle='-', linewidth=2.5, marker='None', markeredgecolor='#2171b5')
 ax[0,1].fill_between(incl_deg, bottom_vpeak2, top_vpeak2, color='#2171b5', alpha=0.3)
 ax[0,1].title.set_text('$[NeII] \, 12.81 \mu m$')
-ax[0,1].axis([-1., 91., -0.5, 17.5])
 ax[0,2].plot(incl_deg, np.abs(v_peak1), color='#6baed6', linestyle='-', linewidth=2.5, marker='None', markeredgecolor='#6baed6')
 ax[0,2].fill_between(incl_deg, bottom_vpeak1, top_vpeak1, color='#6baed6', alpha=0.3)
-ax[0,2].axis([-1., 91., -0.5, 17.5])
+for col in range(3):
+    ax[0,col].errorbar(incl_data2, np.abs(vpeak_data2), yerr=err_vpeak_data2, color='k', linestyle='None', marker='*', capsize=3, label='$Pascucci\,&\,Sterzik\,(2009)$')
+    for i in range(len(name)):
+        ax[0,col].annotate(name[i], (incl_data2[i]+0.3, np.abs(vpeak_data2[i])+0.3))
+    ax[0,col].errorbar(incl_data3, np.abs(vpeak_data3), yerr=err_vpeak_data3, color='k', linestyle='None', marker='d', capsize=3, label='$Baldovin-Saavedra\,(2012)$')
+    for i in range(len(name3)):
+        ax[0,col].annotate(name3[i], (incl_data3[i]-12.0, np.abs(vpeak_data3[i])-0.1))
+    ax[0,col].errorbar(incl_data, np.abs(vpeak_data), yerr=err_vpeak_data, color='k', linestyle='None', marker='o', capsize=3, label='$Sacco\,et\,al.\,(2012)$')
+    for i in range(len(ID)):
+        ax[0,col].annotate(ID[i], (incl_data[i]+0.3, np.abs(vpeak_data[i])+0.3))
+    ax[0,col].axis([-1., 91., -0.5, 17.5])
+    ax[0,col].set_xticks(np.arange(min(incl_deg), max(incl_deg)+10., 10.0))
+    leg2 = ax[0,col].legend(['$c_{s} = '+str(cs[col])+' \, km/s$'], loc='upper left', frameon=False, handlelength=0, handletextpad=0)
+    # for item in leg2.legendHandles:
+    #     item.set_visible(False)
+    ax[0,col].add_artist(leg2)
 ax[1,0].plot(incl_deg, fwhm3, color='#08306b', linestyle='-', linewidth=2.5, marker='None', markeredgecolor='#08306b')
 ax[1,0].fill_between(incl_deg, bottom_fwhm3, top_fwhm3, color='#08306b', alpha=0.3)
 ax[1,0].set_xlabel(r'$i \, [^{\circ}]$')
 ax[1,0].set_ylabel(r'$FWHM \, [km/s]$')
-ax[1,0].axis([-1., 91., 5., 60.])
 ax[1,1].plot(incl_deg, fwhm2, color='#2171b5', linestyle='-', linewidth=2.5, marker='None', markeredgecolor='#2171b5')
 ax[1,1].fill_between(incl_deg, bottom_fwhm2, top_fwhm2, color='#2171b5', alpha=0.3)
-ax[1,1].axis([-1., 91., 5., 60.])
 ax[1,2].plot(incl_deg, fwhm1, color='#6baed6', linestyle='-', linewidth=2.5, marker='None', markeredgecolor='#6baed6')
 ax[1,2].fill_between(incl_deg, bottom_fwhm1, top_fwhm1, color='#6baed6', alpha=0.3)
-ax[1,2].axis([-1., 91., 5., 60.])
-for row in range(2):
-    for col in range(3):
-        ax[row,col].errorbar(incl_data2, np.abs(fwhm_data2), yerr=err_fwhm_data2, color='k', linestyle='None', marker='*', capsize=3, label='$Pascucci\,&\,Sterzik\,(2009)$')
-        for i in range(len(name)):
-            ax[row,col].annotate(name[i], (incl_data2[i]+0.3, np.abs(fwhm_data2[i])+0.3))
-        ax[row,col].errorbar(incl_data3, np.abs(fwhm_data3), yerr=err_fwhm_data3, color='k', linestyle='None', marker='d', capsize=3, label='$Baldovin-Saavedra\,(2012)$')
-        for i in range(len(name3)):
-            ax[row,col].annotate(name3[i], (incl_data3[i]-12.0, np.abs(fwhm_data3[i])-0.1))
-        plt.errorbar(incl_data, np.abs(fwhm_data), yerr=err_fwhm_data, color='k', linestyle='None', marker='o', capsize=3, label='$Sacco\,et\,al.\,(2012)$')
-        for i in range(len(ID)):
-            ax[row,col].annotate(ID[i], (incl_data[i]+0.3, np.abs(fwhm_data[i])+0.3))
-        ax[row,col].set_xticks(np.arange(min(incl_deg), max(incl_deg)+10., 10.0))
-        leg1 = ax[0,0].legend(loc='upper left')
-        leg2 = ax[row,col].legend(['$c_{s} = '+str(cs[row])+' \, km/s$'], loc='upper right', frameon=False, handlelength=0, handletextpad=0)
-        # for item in leg2.legendHandles:
-        #     item.set_visible(False)
-        ax[row,col].add_artist(leg2)
+for col in range(3):
+    ax[1,col].errorbar(incl_data2, np.abs(fwhm_data2), yerr=err_fwhm_data2, color='k', linestyle='None', marker='*', capsize=3, label='$Pascucci\,&\,Sterzik\,(2009)$')
+    for i in range(len(name)):
+        ax[1,col].annotate(name[i], (incl_data2[i]+0.3, np.abs(fwhm_data2[i])+0.3))
+    ax[1,col].errorbar(incl_data3, np.abs(fwhm_data3), yerr=err_fwhm_data3, color='k', linestyle='None', marker='d', capsize=3, label='$Baldovin-Saavedra\,(2012)$')
+    for i in range(len(name3)):
+        ax[1,col].annotate(name3[i], (incl_data3[i]-12.0, np.abs(fwhm_data3[i])-0.1))
+    plt.errorbar(incl_data, np.abs(fwhm_data), yerr=err_fwhm_data, color='k', linestyle='None', marker='o', capsize=3, label='$Sacco\,et\,al.\,(2012)$')
+    for i in range(len(ID)):
+        ax[1,col].annotate(ID[i], (incl_data[i]+0.3, np.abs(fwhm_data[i])+0.3))
+    ax[1,col].axis([-1., 91., 5., 60.])
+    ax[1,col].set_xticks(np.arange(min(incl_deg), max(incl_deg)+10., 10.0))
+    leg2 = ax[1,col].legend(['$c_{s} = '+str(cs[col])+' \, km/s$'], loc='upper left', frameon=False, handlelength=0, handletextpad=0)
+    # for item in leg2.legendHandles:
+    #     item.set_visible(False)
+    ax[1,col].add_artist(leg2)
 # plt.legend(bbox_to_anchor=(0., 1.), loc='upper left', ncol=2)
 plt.subplots_adjust(hspace=0., wspace=0.)
 plt.tight_layout()
