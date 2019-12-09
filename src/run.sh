ebfce16c32370dc2e4d4a255652bd703266ac858
Giulia Ballabio <gb258@leicester.ac.uk>
2019-12-09 18:08:29 +0000

    Minor changes

--- src/run.sh
+++ src/run.sh
@@ -12,10 +12,10 @@ species=NeII # REMEMBER to change it also in line_profile.f90
 mdot=mdot10e-9 # REMEMBER to change it also in line_profile.f90
 
 for ((i=0;i<${#array_b[@]};++i)); do
-	for  cs in 3.0d5 5.0d5 10.0d5; do
+	for cs in 3.0d5 5.0d5 10.0d5; do
 		for incl in 0.0 5.0 10.0 15.0 20.0 25.0 30.0 35.0 40.0 45.0 50.0 55.0 60.0 65.0 70.0 75.0 80.0 85.0 90.0; do
 
-			echo $species" with b="${array_b[i]}", mdot="$mdot " and cs="$cs " for i="$incl
+			echo $species" with b="${array_b[i]}", mdot="$mdot " and cs="$cs[j] " for i="$incl
 
 			# Update the code with the new variables
 			sed -i -e "s/b_input=.*/b_input=${array_b[i]} /g" selfsimilar_solutions.f90
